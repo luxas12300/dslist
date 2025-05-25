596acf6d32975b2e70a05a265fdea94d0f559e4d
Lucas Costa <94561395+luxas12300@users.noreply.github.com>
2025-05-25 18:44:29 -0300

    Game seed

--- dslist/src/main/java/com/lucas/dslist/Game.java
+++ dslist/src/main/java/com/lucas/dslist/Game.java
@@ -5,19 +5,21 @@ import jakarta.persistence.*;
 import java.util.Objects;
 
 @Entity
-@Table(name = "TB_GAME")
+@Table(name = "tb_game")
 public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-    @Column(name = "GAME_YEAR")
+    @Column(name = "game_year")
     private int year;
     private String genre;
     private String platforms;
     private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){}
